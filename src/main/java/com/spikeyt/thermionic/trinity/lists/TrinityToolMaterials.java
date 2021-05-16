7d33679b961614661e1420e1e33d80a763117d16
Nathan Knight <NateLyleKnight@gmail.com>
2021-05-16 16:27:33 -0400

    1.6.1

--- src/main/java/com/spikeyt/thermionic/trinity/lists/TrinityToolMaterials.java
+++ src/main/java/com/spikeyt/thermionic/trinity/lists/TrinityToolMaterials.java
@@ -5,136 +5,93 @@ import net.minecraft.item.Item;
 import net.minecraft.item.crafting.Ingredient;
 
 public enum TrinityToolMaterials implements IItemTier {
-	spearwoodmat(1.8f, 1.0f, 69, 0, 42, TrinityItems.spear_wood),
 	
-	speargoldmat(1.8f, 1.0f, 42, 0, 42, TrinityItems.spear_gold),
+	//Spear
+	spearwoodmat(3.8f, 1.0f, 69, 0, 42, TrinityItems.spear_wood),	
+	speargoldmat(3.8f, 1.0f, 42, 0, 42, TrinityItems.spear_gold),	
+	spearstonemat(4f, 1.0f, 141, 0, 42, TrinityItems.spear_stone),	
+	spearironmat(4.5f, 1.0f, 260, 0, 42, TrinityItems.spear_iron),	
+	speardiamondmat(5f, 1.0f, 1571, 0, 42, TrinityItems.spear_diamond),	
+	spearnetheritemat(5.5f, 1.0f, 2041, 0, 42, TrinityItems.spear_netherite),
 	
-	spearstonemat(2f, 1.0f, 141, 0, 42, TrinityItems.spear_stone),
+	//Battle Axe
+	woodbattleaxemat(8f, 2.0f, 135, 0, 42, TrinityItems.battle_axe_wood),	
+	goldbattleaxemat(8f, 10.0f, 48, 0, 42, TrinityItems.battle_axe_gold),	
+	stonebattleaxemat(10f, 4.0f, 147, 0, 42, TrinityItems.battle_axe_stone),	
+	ironbattleaxemat(10.5f, 6.0f, 266, 0, 42, TrinityItems.battle_axe_iron),		
+	diamondbattleaxemat(11f, 8.0f, 1577, 0, 42, TrinityItems.battle_axe_diamond),	
+	netheritebattleaxemat(12f, 8.0f, 2051, 0, 42, TrinityItems.battle_axe_netherite),
+
+	//War Hammer
+	woodwarhammermat(4.8f, 1.0f, 72, 0, 42, TrinityItems.war_hammer_wood),	
+	goldwarhammermat(4.8f, 1.0f, 45, 0, 42, TrinityItems.war_hammer_gold),	
+	stonewarhammermat(5.4f, 1.0f, 144, 0, 42, TrinityItems.war_hammer_stone),	
+	ironwarhammermat(6.7f, 1.0f, 263, 0, 42, TrinityItems.war_hammer_iron),	
+	diamondwarhammermat(7f, 1.0f, 1574, 0, 42, TrinityItems.war_hammer_diamond),
+	netheritewarhammermat(8.1f, 1.0f, 2040, 0, 42, TrinityItems.war_hammer_netherite),
 	
-	spearironmat(2.5f, 1.0f, 260, 0, 42, TrinityItems.spear_iron),
-	
-	speardiamondmat(3.5f, 1.0f, 1571, 0, 42, TrinityItems.spear_diamond),
-	
-	spearnetheritemat(4.5f, 1.0f, 2041, 0, 42, TrinityItems.spear_netherite),
-	
-	woodbattleaxemat(4.1f, 2.0f, 135, 0, 42, TrinityItems.battle_axe_wood),
-	
-	goldbattleaxemat(4.1f, 10.0f, 48, 0, 42, TrinityItems.battle_axe_gold),
-	
-	stonebattleaxemat(5.5f, 4.0f, 147, 0, 42, TrinityItems.battle_axe_stone),
-	
-	ironbattleaxemat(6.8f, 6.0f, 266, 0, 42, TrinityItems.battle_axe_iron),
-		
-	diamondbattleaxemat(7.8f, 8.0f, 1577, 0, 42, TrinityItems.battle_axe_diamond),
-	
-	netheritebattleaxemat(8.8f, 8.0f, 2051, 0, 42, TrinityItems.battle_axe_netherite),
-	
-	woodwarhammermat(3.8f, 1.0f, 72, 0, 42, TrinityItems.war_hammer_wood),
-	
-	goldwarhammermat(3.8f, 1.0f, 45, 0, 42, TrinityItems.war_hammer_gold),
-	
-	stonewarhammermat(4.4f, 1.0f, 144, 0, 42, TrinityItems.war_hammer_stone),
-	
-	ironwarhammermat(5.7f, 1.0f, 263, 0, 42, TrinityItems.war_hammer_iron),
-	
-	diamondwarhammermat(6f, 1.0f, 1574, 0, 42, TrinityItems.war_hammer_diamond),
-	
-	netheritewarhammermat(7.1f, 1.0f, 2040, 0, 42, TrinityItems.war_hammer_netherite),
-	
-	woodkatanamat(4f, 1.0f, 75, 0, 42, TrinityItems.katana_wood),
-	
-	goldkatanamat(4f, 1.0f, 48, 0, 42, TrinityItems.katana_gold),
-	
-	stonekatanamat(5.2f, 1.0f, 147, 0, 42, TrinityItems.katana_stone),
-	
+	//Katana
+	woodkatanamat(4f, 1.0f, 75, 0, 42, TrinityItems.katana_wood),	
+	goldkatanamat(4f, 1.0f, 48, 0, 42, TrinityItems.katana_gold),	
+	stonekatanamat(5.2f, 1.0f, 147, 0, 42, TrinityItems.katana_stone),	
 	ironkatanamat(6f, 1.0f, 266, 0, 42, TrinityItems.katana_iron),
+	diamondkatanamat(7.2f, 1.0f, 1577, 0, 42, TrinityItems.katana_diamond),
+	netheritekatanamat(8.2f, 1.0f, 2035, 0, 42, TrinityItems.katana_netherite),
 	
-	diamondkatanamat(6.8f, 1.0f, 1577, 0, 42, TrinityItems.katana_diamond),
+	//Knife
+	woodknifemat(2f, 1.0f, 65, 0, 42, TrinityItems.knife_wood),	
+	goldknifemat(2f, 1.0f, 38, 0, 42, TrinityItems.knife_gold),	
+	stoneknifemat(2.5f, 1.0f, 137, 0, 42, TrinityItems.knife_stone),	
+	ironknifemat(3f, 1.0f, 256, 0, 42, TrinityItems.knife_iron),	
+	diamondknifemat(4.5f, 1.0f, 1567, 0, 42, TrinityItems.knife_diamond),	
+	netheriteknifemat(5.5f, 1.0f, 2045, 0, 42, TrinityItems.knife_netherite),
 	
-	netheritekatanamat(7.2f, 1.0f, 2035, 0, 42, TrinityItems.katana_netherite),
+	//Halberd
+	woodhalberdmat(2.5f, 1.0f, 67, 0, 42, TrinityItems.halberd_wood),	
+	goldhalberdmat(2.5f, 1.0f, 40, 0, 42, TrinityItems.halberd_gold),	
+	stonehalberdmat(4.5f, 1.0f, 139, 0, 42, TrinityItems.halberd_stone),	
+	ironhalberdmat(5.5f, 1.0f, 258, 0, 42, TrinityItems.halberd_iron),	
+	diamondhalberdmat(6.5f, 1.0f, 1569, 0, 42, TrinityItems.halberd_diamond),	
+	netheritehalberdmat(7f, 1.0f, 2049, 0, 42, TrinityItems.halberd_netherite),
 	
-	woodknifemat(1.7f, 1.0f, 65, 0, 42, TrinityItems.knife_wood),
-	
-	goldknifemat(1.7f, 1.0f, 38, 0, 42, TrinityItems.knife_gold),
-	
-	stoneknifemat(2.2f, 1.0f, 137, 0, 42, TrinityItems.knife_stone),
-	
-	ironknifemat(3.8f, 1.0f, 256, 0, 42, TrinityItems.knife_iron),
-	
-	diamondknifemat(4.4f, 1.0f, 1567, 0, 42, TrinityItems.knife_diamond),
-	
-	netheriteknifemat(5.6f, 1.0f, 2045, 0, 42, TrinityItems.knife_netherite),
-	
-	woodhalberdmat(2.7f, 1.0f, 67, 0, 42, TrinityItems.halberd_wood),
-	
-	goldhalberdmat(2.7f, 1.0f, 40, 0, 42, TrinityItems.halberd_gold),
-	
-	stonehalberdmat(3.3f, 1.0f, 139, 0, 42, TrinityItems.halberd_stone),
-	
-	ironhalberdmat(5.0f, 1.0f, 258, 0, 42, TrinityItems.halberd_iron),
-	
-	diamondhalberdmat(6.6f, 1.0f, 1569, 0, 42, TrinityItems.halberd_diamond),
-	
-	netheritehalberdmat(7.3f, 1.0f, 2049, 0, 42, TrinityItems.halberd_netherite),
-	
-	wooddaggermat(1.7f, 1.0f, 58, 0, 42, TrinityItems.dagger_wood),
-	
-	golddaggermat(1.7f, 1.0f, 31, 0, 42, TrinityItems.dagger_gold),
-	
-	stonedaggermat(2.2f, 1.0f, 130, 0, 42, TrinityItems.dagger_stone),
-	
-	irondaggermat(3.2f, 1.0f, 249, 0, 42, TrinityItems.dagger_iron),
-	
+	//Dagger
+	wooddaggermat(1.8f, 1.0f, 58, 0, 42, TrinityItems.dagger_wood),	
+	golddaggermat(1.8f, 1.0f, 31, 0, 42, TrinityItems.dagger_gold),	
+	stonedaggermat(2.5f, 1.0f, 130, 0, 42, TrinityItems.dagger_stone),	
+	irondaggermat(3.5f, 1.0f, 249, 0, 42, TrinityItems.dagger_iron),	
 	diamonddaggermat(4.5f, 1.0f, 1560, 0, 42, TrinityItems.dagger_diamond),
+	netheritedaggermat(5.5f, 1.0f, 2030, 0, 42, TrinityItems.dagger_netherite),
 	
-	netheritedaggermat(5.4f, 1.0f, 2030, 0, 42, TrinityItems.dagger_netherite),
-	
-	woodlancemat(1.5f, 1.0f, 75, 0, 42, TrinityItems.lance_wood),
-	
+	//Lance
+	woodlancemat(2.5f, 1.0f, 75, 0, 42, TrinityItems.lance_wood),
 	goldlancemat(2.5f, 1.0f, 48, 0, 42, TrinityItems.lance_gold),
+	stonelancemat(3f, 1.0f, 147, 0, 42, TrinityItems.lance_stone),	
+	ironlancemat(4.5f, 1.0f, 249, 0, 42, TrinityItems.lance_iron),	
+	diamondlancemat(5f, 1.0f, 1570, 0, 42, TrinityItems.lance_diamond),	
+	netheritelancemat(6f, 1.0f, 2041, 0, 42, TrinityItems.lance_netherite),
 	
-	stonelancemat(3.8f, 1.0f, 147, 0, 42, TrinityItems.lance_stone),
+	//Guisarme
+	woodguisarmemat(3.5f, 1.0f, 60, 0, 42, TrinityItems.guisarme_wood),	
+	goldguisarmemat(3.5f, 1.0f, 33, 0, 42, TrinityItems.guisarme_gold),	
+	stoneguisarmemat(4.5f, 1.0f, 132, 0, 42, TrinityItems.guisarme_stone),	
+	ironguisarmemat(5.5f, 1.0f, 251, 0, 42, TrinityItems.guisarme_iron),	
+	diamondguisarmemat(6.5f, 1.0f, 1570, 0, 42, TrinityItems.guisarme_diamond),	
+	netheriteguisarmemat(7.5f, 1.0f, 2041, 0, 42, TrinityItems.guisarme_netherite),
 	
-	ironlancemat(4.2f, 1.0f, 249, 0, 42, TrinityItems.lance_iron),
+	//Lucerne
+	woodlucernemat(2.5f, 1.0f, 70, 0, 42, TrinityItems.lucerne_hammer_wood),	
+	goldlucernemat(2.5f, 1.0f, 43, 0, 42, TrinityItems.lucerne_hammer_gold),	
+	stonelucernemat(3.5f, 1.0f, 142, 0, 42, TrinityItems.lucerne_hammer_stone),	
+	ironlucernemat(5f, 1.0f, 261, 0, 42, TrinityItems.lucerne_hammer_iron),	
+	diamondlucernemat(6f, 1.0f, 1580, 0, 42, TrinityItems.lucerne_hammer_diamond),	
+	netheritelucernemat(7.2f, 1.0f, 2061, 0, 42, TrinityItems.lucerne_hammer_netherite),
 	
-	diamondlancemat(4.5f, 1.0f, 1570, 0, 42, TrinityItems.lance_diamond),
-	
-	netheritelancemat(5.1f, 1.0f, 2041, 0, 42, TrinityItems.lance_netherite),
-	
-	woodguisarmemat(3.5f, 1.0f, 60, 0, 42, TrinityItems.guisarme_wood),
-	
-	goldguisarmemat(3.5f, 1.0f, 33, 0, 42, TrinityItems.guisarme_gold),
-	
-	stoneguisarmemat(4.5f, 1.0f, 132, 0, 42, TrinityItems.guisarme_stone),
-	
-	ironguisarmemat(5.4f, 1.0f, 251, 0, 42, TrinityItems.guisarme_iron),
-	
-	diamondguisarmemat(6.3f, 1.0f, 1570, 0, 42, TrinityItems.guisarme_diamond),
-	
-	netheriteguisarmemat(7.2f, 1.0f, 2041, 0, 42, TrinityItems.guisarme_netherite),
-	
-	woodlucernemat(2.5f, 1.0f, 70, 0, 42, TrinityItems.lucerne_hammer_wood),
-	
-	goldlucernemat(2.5f, 1.0f, 43, 0, 42, TrinityItems.lucerne_hammer_gold),
-	
-	stonelucernemat(3.5f, 1.0f, 142, 0, 42, TrinityItems.lucerne_hammer_stone),
-	
-	ironlucernemat(4.5f, 1.0f, 261, 0, 42, TrinityItems.lucerne_hammer_iron),
-	
-	diamondlucernemat(6.5f, 1.0f, 1580, 0, 42, TrinityItems.lucerne_hammer_diamond),
-	
-	netheritelucernemat(7.5f, 1.0f, 2061, 0, 42, TrinityItems.lucerne_hammer_netherite),
-	
-	woodquarterstaffmat(1.5f, 1.0f, 58, 0, 42, TrinityItems.quarterstaff_wood),
-	
-	goldquarterstaffmat(1.5f, 1.0f, 38, 0, 42, TrinityItems.quarterstaff_gold),
-	
-	stonequarterstaffmat(1.8f, 1.0f, 137, 0, 42, TrinityItems.quarterstaff_stone),
-	
-	ironquarterstaffmat(2.5f, 1.0f, 275, 0, 42, TrinityItems.quarterstaff_iron),
-	
-	diamondquarterstaffmat(3.5f, 1.0f, 1569, 0, 42, TrinityItems.quarterstaff_diamond),
-	
+	//Quarterstaff
+	woodquarterstaffmat(1.5f, 1.0f, 58, 0, 42, TrinityItems.quarterstaff_wood),	
+	goldquarterstaffmat(1.5f, 1.0f, 38, 0, 42, TrinityItems.quarterstaff_gold),	
+	stonequarterstaffmat(2.5f, 1.0f, 137, 0, 42, TrinityItems.quarterstaff_stone),	
+	ironquarterstaffmat(3f, 1.0f, 275, 0, 42, TrinityItems.quarterstaff_iron),	
+	diamondquarterstaffmat(3.5f, 1.0f, 1569, 0, 42, TrinityItems.quarterstaff_diamond),	
 	netheritequarterstaffmat(4.5f, 1.0f, 2049, 0, 42, TrinityItems.quarterstaff_netherite);
 	
 	
